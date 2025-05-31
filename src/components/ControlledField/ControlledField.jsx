a66370deab9ada113681b5f326ab681e9c95f217
SHANTO <rabbymd55555@gmail.com>
2025-06-01 04:56:54 +0600

    react form

--- src/components/ControlledField/ControlledField.jsx
+++ src/components/ControlledField/ControlledField.jsx
@@ -2,12 +2,27 @@ import React, { useState } from 'react';
 
 const ControlledField = () => {
     const [password,setPassword] = useState('')
+    const [error,setError] = useState('')
     const handleSubmit = (e) =>{
         e.preventDefault()
         console.log('shanto');
+        if(password.length <6){
+            setError('error')
+        }
+        else{
+            setError('')
+        }
     }
     const handleOnSubmit = e =>{
         console.log(e.target.value);
+        setPassword(e.target.value)
+
+        // if(password.length < 6){
+        //     setError('Password must be 6 characters or longer.')
+        // }
+        // else{
+        //     setError('')
+        // }
     }
     return (
         <div>
@@ -19,6 +34,9 @@ const ControlledField = () => {
                 <br />
                 <input type="submit" value="Submit" />
             </form>
+            <p style={{color:'red'}}>
+                <small>{error}</small>
+            </p>
         </div>
     );
 };
