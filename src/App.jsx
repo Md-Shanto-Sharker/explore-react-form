a4336a18985a03406c892050108527caf249535e
SHANTO <rabbymd55555@gmail.com>
2025-09-10 22:09:24 +0600

    added react simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import ControledFiled from "./components/ControledFiled/ControledFiled";
+import FormAction from "./components/FormAction/FormAction";
 import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
   return (
     <>
       <h1>Explore React Form</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <FormAction></FormAction> */}
+      <ControledFiled></ControledFiled>
     </>
   );
 }
