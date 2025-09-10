f1b434c906d9024618c433cdb35a5067984b738e
SHANTO <rabbymd55555@gmail.com>
2025-09-10 20:20:14 +0600

    added react simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import "./App.css";
-import ControlledField from "./components/ControlledField/ControlledField";
-import FormAction from "./components/FormAction/FormAction";
 import SimpleForm from "./components/SimpleForm/SimpleForm";
-import SimpleForm2 from "./components/SimpleForm2/SimpleForm2";
 
 function App() {
   return (
     <>
       <h1>Explore React Form</h1>
-      {/* <SimpleForm></SimpleForm> */}
-      {/* <SimpleForm2></SimpleForm2> */}
-
-      {/* <FormAction></FormAction> */}
-
-      <ControlledField></ControlledField>
+      <SimpleForm></SimpleForm>
     </>
   );
 }
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,23 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const SimpleForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("click");
-    console.log(e.target.name.value);
-    console.log(e.target.email.value);
-  };
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="name" />
-        <br />
-        <input type="email" name="email" id="" />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            
+        </div>
+    );
 };
 
 export default SimpleForm;
